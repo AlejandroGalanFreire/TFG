6a4a001de2df408368f954217dc9ad5fda150b18
AlejandroGalanFreire <UO277346@uniovi.es>
2023-04-16 22:56:59 +0200

    gameStats segunda version

--- Proyecto/frontend/nbaAnalitics_fr/src/app/main/main/matchs/matchs/matchs.component.ts
+++ Proyecto/frontend/nbaAnalitics_fr/src/app/main/main/matchs/matchs/matchs.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { GameStats } from 'src/app/models/gameStats';
 import { DataService } from 'src/app/services/data-service.service';
 
@@ -9,16 +11,37 @@ import { DataService } from 'src/app/services/data-service.service';
 })
 export class MatchsComponent {
 
-  gamesStats: GameStats[] = [];
-  selectedDate: string = "2023-04-09";
+  gamesStats: Array<GameStats[]> = new Array();
+  currentDateSubject: BehaviorSubject<Date> = new BehaviorSubject<Date>(new Date("2023-04-09")); // fecha de fin de temporada regular
+  currentDate: Observable<Date> = this.currentDateSubject.asObservable();
+  selectedDate: string = "";
 
   constructor(private dataService: DataService){}
 
   ngOnInit(): void {
-    this.dataService.getGamesStatsByDate(this.selectedDate).subscribe(
-      (resp: any) => {
-        this.gamesStats = JSON.parse(resp)
-      }
-    );
+    this.currentDate.subscribe(date => {
+      this.selectedDate = date.getFullYear() + "-" + this.formatMonthAndDay(date.getMonth() + 1) + "-"
+      + this.formatMonthAndDay(date.getDate());
+      this.dataService.getGamesStatsByDate(this.selectedDate).subscribe(
+        (resp: any) => {
+          this.gamesStats = JSON.parse(resp)
+          console.log(this.gamesStats[0][0].team_abbreviation);
+        }
+      );
+    });
+
+  }
+
+  dateEvent(event: MatDatepickerInputEvent<Date>) {
+    if(event.value){
+      this.currentDateSubject.next(event.value);
+    }
+  }
+
+  formatMonthAndDay(numero: number): string{
+    if(numero < 10){
+      return "0" + numero;
+    }
+    return numero.toString();
   }
 }
