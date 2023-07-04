9559acea801d798d4bcb5a8b79693f5314d9e64d
AlejandroGalanFreire <79645666+AlejandroGalanFreire@users.noreply.github.com>
2023-07-04 17:31:38 +0200

    Delete Proyecto/backend/demo directory

--- Proyecto/backend/demo/src/main/java/com/example/demo/games/model/Periods.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.games.model;
-
-import com.example.demo.commonstats.CommonPeriods;
-
-public class Periods extends CommonPeriods{
-	
-}
--- Proyecto/backend/demo/src/main/java/com/example/demo/league/service/LeagueService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.league.service;
-
-import com.example.demo.league.model.League;
-
-public interface LeagueService {
-
-	League getLeague(String leagueName);
-}
